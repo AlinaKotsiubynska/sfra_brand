56a83d0d728cd4729dc6108ebf8b4e51e0e269e5
Alina_Kotsiubynska <alina.kotsiubynska@valtech.com>
2022-02-21 16:18:55 +0200

    +add: build script, integrates notifyme plugin

--- cartridges/app_onboarding/cartridge/client/default/js/productDetail.js
+++ cartridges/app_onboarding/cartridge/client/default/js/productDetail.js
@@ -6,4 +6,6 @@ $(document).ready(function () {
     processInclude(require('plugin_wishlists/product/details'));
     processInclude(require('plugin_wishlists/product/wishlist'));
     processInclude(require('plugin_giftregistry/product/giftRegistry'));
+    processInclude(require('plugin_notifyme/product/base'));
+    processInclude(require('plugin_notifyme/product/notifyMe'));
 });
